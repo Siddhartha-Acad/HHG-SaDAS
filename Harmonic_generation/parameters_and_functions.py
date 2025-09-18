5d5a0e905d72ed7316e3d5de698595eb7543e39c
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-09-18 22:52:48 +0530

    Ready to implement the check_Evolution_dt.py

--- Harmonic_generation/parameters_and_functions.py
+++ Harmonic_generation/parameters_and_functions.py
@@ -35,7 +35,7 @@ print('*** DeprecationWarning : Blocked from parameters_and_functions.py ***\n')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #           Atom, SAE and Confinement            |
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-n = 2; l = 1; m = 0        # defines initial state. [NOTE]: for a given l, n always starts from 1. Ex: 1s=(1, 0, 0); 2pz=(1, 1, 0); 4px=(3, 1, 1)
+n = 1; l = 0; m = 0        # defines initial state. [NOTE]: for a given l, n always starts from 1. Ex: 1s=(1, 0, 0); 2pz=(1, 1, 0); 4px=(3, 1, 1)
 evolving_atom = 'He'       # Atoms are listed down in 'SAE dataset' section.
 SAE_model = 'SAE-M1'       # Single active electron model; option: SAE_model = 'SAE-M1' or 'SAE-M2'. [NOTE]: For 'Xe' always use 'SAE-M1'
 
