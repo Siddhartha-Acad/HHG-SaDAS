6cb73ec44441d51d3dcd926df3cb713c354a1d2a
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-09-30 17:51:02 +0530

    [HHG_Spectra] Fourth version
    - added comments and comment sections.
    - plot looks better now.

--- Assistant/Decorate_axes/decorate_axes_L_thesis.py
+++ Assistant/Decorate_axes/decorate_axes_L_thesis.py
@@ -29,6 +29,7 @@ Notes:
 """
 
 import numpy as np
+from cycler import cycler
 from Assistant.Color_schemes import manim_colors as mc
 
 dec_color = np.concatenate((mc.C_L_reordered, mc.des_col_1))
--- Harmonic_generation/HHG_spectra_analysis/HHG_spectra.py
+++ Harmonic_generation/HHG_spectra_analysis/HHG_spectra.py
@@ -15,60 +15,84 @@ import pandas as pd
 import scipy.fft as ft
 from pathlib import Path
 import matplotlib.pyplot as plt
-from Assistant.Decorate_axes import decorate_axes_D as da
+from Assistant.Decorate_axes import decorate_axes_L_thesis as da
 from Harmonic_generation.parameters_and_functions import dt, tf, w0, T
+
+# ~~~~~~~~~~~~~~~~~~~~~~: Common Figure Settings :~~~~~~~~~~~~~~~~~~~~~
+width = 6.2                         # Width in inches
+height = 4                          # Height in inches
+fig_scale_factor = 2                # big=2 ; medium=1.5; small=1
+fig_size = (fig_scale_factor*width, fig_scale_factor*height)
+
+plt.rc('font', **{'family': 'serif', 'size': 14})
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                Import and read time evolution data                 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 this_dir = Path(__file__).resolve().parent
 
-dip_mom_file = 'Evo_steps=88036_He(1s)_m=0_SAE-M1__L=20_kmax=50_N=200_rmax=200_Lmap=80_dt=0.1.xlsx'
-file_dip_mom = this_dir.parent / 'Time_evolution_data' / f'{dip_mom_file}'
-file_dip_mom = pd.read_excel(file_dip_mom, header=None, skiprows=1).to_numpy().T
+evo_data_file_name = 'Evo_steps=88036_He(1s)_m=0_SAE-M1__L=20_kmax=50_N=200_rmax=200_Lmap=80_dt=0.1.xlsx'
+evo_data_file_path = this_dir.parent / 'Time_evolution_data' / f'{evo_data_file_name}'
+evo_data = pd.read_excel(evo_data_file_path, header=None, skiprows=1).to_numpy().T
 
-t = file_dip_mom[0]; E_t = file_dip_mom[1]; dipole_moment = file_dip_mom[2]
+t = evo_data[0]                  # time               : (a.u.)
+E_t = evo_data[1]                # Electric field     : (a.u.)
+dipole_moment = evo_data[2]      # dipole moment d(t) : (a.u.)
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                    Calculating the HHG Spectra                     |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dipole_mom_FFT = ft.fft(dipole_moment)
-freq_w = ft.fftfreq(len(t), d=dt) * 2 * np.pi                   # Angular frequency: w=[-oo, oo]
-dip_mom_power_spectra = np.abs(dipole_mom_FFT / tf)**2          # Harmonic power : dipole mom
-positive_freq_mask = freq_w > 0
-freq_pos = freq_w[positive_freq_mask]
+freq_w = ft.fftfreq(len(t), d=dt) * 2 * np.pi                   # Nyquist Angular frequency (omega -> w).
+dip_mom_power_spectra = np.abs(dipole_mom_FFT / tf)**2          # Harmonic power spectra.
+
+positive_freq_mask = freq_w > 0             # Select positive angular frequencies; returns a boolean array where True corresponds to freq_w > 0
+freq_pos = freq_w[positive_freq_mask]       # Positive Nyquist frequencies.
+
 harmonic_order = freq_pos / w0                                          # Harmonic Order (w/w0)
-dip_mom_power_spectra = dip_mom_power_spectra[positive_freq_mask]       # P(w) : dipole mom
+dip_mom_power_spectra = dip_mom_power_spectra[positive_freq_mask]       # P(w) : The power spectra you want!
 
 
-# ~~~~~~~~: Conversion efficiency :~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                       Conversion efficiency                        |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 harmonic_energy = np.trapezoid(dip_mom_power_spectra, freq_pos)
 laser_energy = np.trapezoid(E_t**2, t)
 
 eta = harmonic_energy / laser_energy
 print(f"Conversion efficiency: {eta:.10f}")
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~: Plotting :~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-fig = plt.figure()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Plotting                              |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+fig = plt.figure(figsize=fig_size)
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
 da.decorate_2d([ax1, ax2])
 
-ax1.plot(t/T, dipole_moment, lw=2, label='d(t)')
-ax2.plot(harmonic_order, np.log10(dip_mom_power_spectra), label=r'log$_{10}$(P($\omega$)) ~ d(t)')
+ax1.plot(t/T, dipole_moment, color='#7F3FBF', lw=2, label='d(t)')
+ax2.plot(harmonic_order, np.log10(dip_mom_power_spectra), color='#2CA02C', label=r'log$_{10}$(P($\omega$))')
 
-ax1.set_xticks(np.arange(0, 61, 10))
-ax2.set_xticks(np.arange(1, int(max(harmonic_order)), 2))
-ax2.set_yticks(np.arange(-20, -2, 4))
+ax1.set_xticks(np.arange(0, 61, 5))                                # Some controls
+ax2.set_yticks(np.arange(-20, -2, 4))                               # over
+ax2.set_xticks(np.arange(1, int(max(harmonic_order)), 4))           # dipole moment
+ax2.set_xlim(-1, 102)                                               # and
+ax1.set_ylim(1.5 * min(dipole_moment), 1.5 * max(dipole_moment))    # HHG spectra plot.
 
-ax1.set_ylim(1.5 * min(dipole_moment), 1.5 * max(dipole_moment)); ax2.set_xlim(-1, 102)
+ax1.set_xlabel('Optical Cycles (t/T)', fontsize=15)
+ax1.set_ylabel('d(t)', fontsize=15)
+ax2.set_xlabel(r'Harmonic Order (ω/ω$_0$)', fontsize=15)
+ax2.set_ylabel(r'log$_{10}$[P($\omega$)]', fontsize=15)
 
-ax1.set_xlabel(r't/T :$\longrightarrow$', fontsize=15)
-ax1.set_ylabel(r'd(t) :$\longrightarrow$', fontsize=15)
-ax2.set_xlabel(r'ω/ω$_0$ :$\longrightarrow$', fontsize=15)
-ax2.set_ylabel(r'log$_{10}$(P($\omega$)) :$\longrightarrow$', fontsize=15)
+ax1.legend(loc='upper right', fontsize=14, framealpha=0.5, edgecolor='w')
+ax2.legend(loc='upper right', fontsize=14, framealpha=0.5, edgecolor='w')
 
-ax1.legend(loc='upper right', fontsize=12, framealpha=0.5, edgecolor='k')
-ax2.legend(loc='upper right', fontsize=12, framealpha=0.5, edgecolor='k')
-
-fig.suptitle(dip_mom_file, fontsize=12)
-fig.subplots_adjust(top=0.94, bottom=0.069, right=0.97, left=0.048, hspace=0.14)
+fig.suptitle(evo_data_file_name, fontsize=12)
+fig.subplots_adjust(top=0.94, bottom=0.069, right=0.98, left=0.079, hspace=0.195)
 
 
 plt.show()
