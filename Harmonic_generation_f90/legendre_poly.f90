004c98b20ac14660b4ba2374b01e294cdb960b9c
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-07 07:35:20 +0530

    included limiting situations where N = 0 or, 1

--- Harmonic_generation_f90/legendre_poly.f90
+++ Harmonic_generation_f90/legendre_poly.f90
@@ -6,7 +6,7 @@ program legender_poly
     real (kind=8) :: x
     real (kind=8) :: legendre_p
 
-    N = 20
+    N = 0
     x = 0.5d0
 
     print *, legendre_p(N, x)
@@ -23,6 +23,14 @@ real (kind=8) function legendre_p(n, x)
 
     P0 = 1.0d0
     P1 = x
+
+    if (n==0) then
+        legendre_p = 1.0d0
+        return
+    else if (n==1) then
+        legendre_p = x
+        return
+    end if
 
     do k = 2, n
         Pk = ((2*k-1)*x*P1 - (k-1)*P0) / k
