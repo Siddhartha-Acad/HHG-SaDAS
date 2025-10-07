59130ed3ebe4474e76f81400315c4297f6133945
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-07 07:28:15 +0530

    made fortran function to compute Pn(x).

--- Harmonic_generation_f90/legendre_poly.f90
+++ Harmonic_generation_f90/legendre_poly.f90
@@ -1,25 +1,34 @@
-! computing legendre polynomial at a given x.
+! legendre polynomial at a given x.
 
 program legender_poly
     implicit none
+    integer :: N
+    real (kind=8) :: x
+    real (kind=8) :: legendre_p
 
+    N = 20
+    x = 0.5d0
+
+    print *, legendre_p(N, x)
+
+end program legender_poly
+
+
+real (kind=8) function legendre_p(n, x)
+    implicit none
     real (kind=8) :: x
     real (kind=8) :: Pk
     real (kind=8) :: P0, P1
-    integer :: k, N = 10
-
-    x = 0.5d0
+    integer :: k, n
 
     P0 = 1.0d0
     P1 = x
 
-    do k = 2, N
+    do k = 2, n
         Pk = ((2*k-1)*x*P1 - (k-1)*P0) / k
-
         P0 = P1
         P1 = Pk
     end do
 
-    print *, Pk
-
-end program legender_poly
+    legendre_p = Pk
+end function legendre_p
