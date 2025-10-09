08e7e237d855823ba18add51934f10d022ef022f
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-09 13:38:04 +0530

    Miller's Algorithm : Demonstrated backward recursion stability !!
    - deleted the separated backward bessel script and moved to bessel_fnc.f90

--- Harmonic_generation_f90/bessel_fnc.f90
+++ Harmonic_generation_f90/bessel_fnc.f90
@@ -1,17 +1,23 @@
-! Bessel function at a given x :: FORWARD RECURSION ALGORITHM
+! Bessel function :: J_{n}(x)
+
+! FORWARD RECURSION EQUATION
 ! J_{n}(x) = \frac{2(n-1)}{x} J_{n-1}(x) - J_{n-2}(x)
+
+! BACKWARD RECURSION EQUATION
+! J_{n-1}(x) = \frac{2n}{x} J_n(x) - J_{n+1}(x)
+
 
 program main
     implicit none
-    integer :: n_val, N = 20     ! Forward recursion stable if (n < x)
-    real (kind=8) :: x = 50.0d0
-    real (kind=8) :: bessel_fwd
-    external :: bessel_fwd
+    integer :: n_val, N = 27                 ! Forward recursion stable if (n < x)
+    real (kind=8) :: x = 5.0d0
+    real (kind=8) :: bessel_fwd,bessel_bwd
+    external :: bessel_fwd, bessel_bwd
 
-    open(unit=10, file='bessel_fwd.dat', status='replace', action='write')
+    open(unit=10, file='bessel_funcn.dat', status='replace', action='write')
 
     do n_val = 1, N
-        write (10, *) n_val, bessel_fwd(n_val, x), BESSEL_JN(n_val, x)
+        write (10, *) n_val, bessel_fwd(n_val, x), bessel_bwd(n_val, x), BESSEL_JN(n_val, x)
     end do
 
     close(10)
@@ -37,7 +43,7 @@ pure real (kind=8) function bessel_fwd(n, x)
     end if
 
     do k = 2, n
-        Jk = (2*(k-1) / x) * J1 - J0
+        Jk = (2.0d0 * real(k-1, kind=8) / x) * J1 - J0
 
         J0 = J1
         J1 = Jk
@@ -45,3 +51,33 @@ pure real (kind=8) function bessel_fwd(n, x)
 
     bessel_fwd = Jk
 end function bessel_fwd
+
+
+
+pure real (kind=8) function bessel_bwd(n, x)
+    implicit none
+    integer :: k, Nmax
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    real (kind=8) :: J_Nmax, J_Nmax_1      ! J_Nmax = J_{Nmax} ; J_Nmax_1 = J_{Nmax+1}
+    real (kind=8) :: JN, Jk                ! JN = J_{N}(x) ; Jk = J_{N-1}(x)
+
+    J_Nmax = 1.0d0
+    J_Nmax_1 = 0.0d0
+
+    Nmax = n + 100
+    do k = Nmax, 1, -1
+        Jk = (2.0d0 * real(k, kind=8) / x) * J_Nmax - J_Nmax_1
+
+        if (k == n) then
+            JN = J_Nmax
+        end if
+
+        J_Nmax_1 = J_Nmax
+        J_Nmax = Jk
+    end do
+
+    JN = (BESSEL_J0(x) / Jk) * JN          ! norm_fact = BESSEL_J0(x) / Jk
+
+    bessel_bwd = JN
+end function bessel_bwd
