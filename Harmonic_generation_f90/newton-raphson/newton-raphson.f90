dee1394b3e3008100448a867c91b1d83fef77b40
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-10 15:17:20 +0530

    moved into a function

--- Harmonic_generation_f90/newton-raphson/newton-raphson.f90
+++ Harmonic_generation_f90/newton-raphson/newton-raphson.f90
@@ -2,19 +2,28 @@
 
 program main
     implicit none
-    integer :: n
-    real (kind=8) :: x_i, x_n      ! init_guess, iteration_val
+    real (kind=8) :: x_i
+    real (kind=8) :: newton_raphson
 
     x_i = 3.0d0
 
+    print *, newton_raphson(x_i)
+
+end program main
+
+
+
+pure function newton_raphson(x_i) result(root)
+    implicit none
+    real (kind=8), intent(in) :: x_i        ! initial guess value
+    real (kind=8) :: root
+    real (kind=8) :: x_n
+    integer :: n
+
     do n = 1, 10
         x_n = x_i - sin(x_i) / cos(x_i)
-
         x_i = x_n
     end do
 
-    print *, x_i, 
-
-
-
-end program main
+    root = x_i
+end function newton_raphson
