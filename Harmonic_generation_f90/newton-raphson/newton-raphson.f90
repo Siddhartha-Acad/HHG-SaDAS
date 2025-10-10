41493a74bf452b4bbcc1fe3f7e89b3e7e6d5bdc1
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-10 13:35:08 +0530

    first working prototype.
    - tested for f(x) = sin(x)

--- Harmonic_generation_f90/newton-raphson/newton-raphson.f90
+++ Harmonic_generation_f90/newton-raphson/newton-raphson.f90
@@ -1,1 +1,20 @@
 ! Newton-Raphson method of root finding.
+
+program main
+    implicit none
+    integer :: n
+    real (kind=8) :: x_i, x_n      ! init_guess, iteration_val
+
+    x_i = 3.0d0
+
+    do n = 1, 10
+        x_n = x_i - sin(x_i) / cos(x_i)
+
+        x_i = x_n
+    end do
+
+    print *, x_i, 
+
+
+
+end program main
