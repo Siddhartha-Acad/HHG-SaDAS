04029f2c0e822658c79bb2c63fea3b649df1e64e
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-10 20:25:26 +0530

    Added debug flag and explicit interface for Newton-Raphson function
    - Introduced optional `debug` flag to print iteration values
    - Written explicit interface so main program can safely call function with optional arguments

--- Harmonic_generation_f90/newton-raphson/newton-raphson.f90
+++ Harmonic_generation_f90/newton-raphson/newton-raphson.f90
@@ -2,36 +2,49 @@
 
 program main
     implicit none
-    real (kind=8) :: x_i
-    real (kind=8) :: root, newton_raphson
+    real (kind=8) :: x_i, root
 
-    x_i = 0.5d0
+    interface
+        function newton_raphson(x_i, debug) result(root)
+            real (kind=8), intent(in) :: x_i
+            logical, intent(in), optional :: debug
+            real (kind=8) :: root
+        end function newton_raphson
+    end interface
 
-    root = newton_raphson(x_i)
+    x_i = 2.5
 
+    root = newton_raphson(x_i, debug=.true.)
     print *, 'root = ', root
 
 end program main
 
 
 
-function newton_raphson(x_i) result(root)
+function newton_raphson(x_i, debug) result(root)
     implicit none
     real (kind=8), intent(in) :: x_i        ! initial guess value
+    logical, intent(in), optional :: debug
     real (kind=8) :: root, x_old, x_new
-    real (kind=8) :: tol, rtol, rel_err
+    real (kind=8) :: tol, rtol
     integer :: n
 
-    tol = 1.0d-14
-    rtol = 0.0d0
-
+    tol = 1.0d-14       ! absolute error tolerance
+    rtol = 0.0d0        ! relative tolerance
     x_old = x_i
-    print '(a3, 2a20, a25)', 'n', 'x_old', 'x_new', 'err'
+
+    if (present(debug)) then
+        if (debug) print '(A3, 2A20, A25)', &
+            'n', 'x_n', 'x_{n+1}', 'err = |x_{n+1} - x_n|'
+    end if
 
     do n = 1, 100
         x_new = x_old - sin(x_old) / cos(x_old)
 
-        print '(I3, 2F20.16, E25.16)', n, x_old, x_new, abs(x_new - x_old)
+        if (present(debug)) then
+            if (debug) print '(I3, 2F20.16, E25.16)', &
+                n, x_old, x_new, abs(x_new - x_old)
+        end if
 
         if (abs(x_new - x_old) .lt. (tol + rtol*abs(x_new))) then
             root = x_new
