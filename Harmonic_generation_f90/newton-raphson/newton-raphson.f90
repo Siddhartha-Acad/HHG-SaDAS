efb444e36f4554d8e894852d940b2eb677549a50
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-10 16:10:59 +0530

    added digit based tolerance limit.
    - it prints the old, new values and relative errors.
    - I had to remove the pure. as pure do not allow printing.

--- Harmonic_generation_f90/newton-raphson/newton-raphson.f90
+++ Harmonic_generation_f90/newton-raphson/newton-raphson.f90
@@ -3,27 +3,42 @@
 program main
     implicit none
     real (kind=8) :: x_i
-    real (kind=8) :: newton_raphson
+    real (kind=8) :: root, newton_raphson
 
     x_i = 3.0d0
 
-    print *, newton_raphson(x_i)
+    print '(a3, 3a20)', 'n', 'x_old', 'x_new', 'rel_err'
+    root = newton_raphson(x_i)
 
 end program main
 
 
 
-pure function newton_raphson(x_i) result(root)
+function newton_raphson(x_i) result(root)
     implicit none
     real (kind=8), intent(in) :: x_i        ! initial guess value
-    real (kind=8) :: root
-    real (kind=8) :: x_n
+    real (kind=8) :: root, x_old, x_new
+    real (kind=8) :: tol, rel_err
     integer :: n
 
+    tol = 1.0d-4
+    rel_err = 1.0d0
+    x_old = x_i
+
     do n = 1, 10
-        x_n = x_i - sin(x_i) / cos(x_i)
-        x_i = x_n
+        x_new = x_old - sin(x_old) / cos(x_old)
+
+        rel_err = abs(x_new - x_old) / abs(x_new)
+
+        print '(I3, 2F20.16, E25.16)', n, x_old, x_new, rel_err
+
+        if (rel_err < tol) then
+            root = x_new
+            return
+        end if
+
+        x_old = x_new
     end do
 
-    root = x_i
+    root = x_old
 end function newton_raphson
