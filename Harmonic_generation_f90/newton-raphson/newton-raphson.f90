8a502e7bf0a33af02df7e4f2f6eab540342e7732
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-10 16:42:56 +0530

    implemented do while loop.
    if this not nonverged then it will run upto 100 iteration

--- Harmonic_generation_f90/newton-raphson/newton-raphson.f90
+++ Harmonic_generation_f90/newton-raphson/newton-raphson.f90
@@ -5,9 +5,8 @@ program main
     real (kind=8) :: x_i
     real (kind=8) :: root, newton_raphson
 
-    x_i = 3.0d0
+    x_i = 2.0d0
 
-    print '(a3, 3a20)', 'n', 'x_old', 'x_new', 'rel_err'
     root = newton_raphson(x_i)
 
 end program main
@@ -19,25 +18,23 @@ function newton_raphson(x_i) result(root)
     real (kind=8), intent(in) :: x_i        ! initial guess value
     real (kind=8) :: root, x_old, x_new
     real (kind=8) :: tol, rel_err
-    integer :: n
+    integer :: n = 1
 
-    tol = 1.0d-4
+    tol = 1.0d-14
     rel_err = 1.0d0
     x_old = x_i
 
-    do n = 1, 10
+    print '(a3, 3a20)', 'n', 'x_old', 'x_new', 'rel_err'
+
+    do while ((rel_err .gt. tol) .and. (n .lt. 100))
         x_new = x_old - sin(x_old) / cos(x_old)
 
         rel_err = abs(x_new - x_old) / abs(x_new)
 
         print '(I3, 2F20.16, E25.16)', n, x_old, x_new, rel_err
 
-        if (rel_err < tol) then
-            root = x_new
-            return
-        end if
-
         x_old = x_new
+        n = n + 1
     end do
 
     root = x_old
