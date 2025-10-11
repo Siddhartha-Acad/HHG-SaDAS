9502975d3f02bc1ca181c8008cd0150b777a9d82
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-11 13:05:58 +0530

    implemented Lambda(n, x) function
    - inserted legendre function.

--- Harmonic_generation_f90/collocation_points/Algo-3.f90
+++ Harmonic_generation_f90/collocation_points/Algo-3.f90
@@ -11,6 +11,47 @@ program main
 end program main
 
 
+pure real (kind=8) function legendre(n, x)
+    ! FORWARD RECURSION ALGORITHM
+    ! P_n(x) = \frac{1}{n}\left[ (2n-1)xP_{n-1}(x) - (n-1)P_{n-2}(x) \right]
+
+    implicit none
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    real (kind=8) :: P0, P1, Pk
+    integer :: k
+
+    P0 = 1.0d0
+    P1 = x
+
+    if (n==0) then
+        legendre = 1.0d0
+        return
+    else if (n==1) then
+        legendre = x
+        return
+    end if
+
+    do k = 2, n
+        Pk = ((2*k-1)*x*P1 - (k-1)*P0) / k
+        P0 = P1
+        P1 = Pk
+    end do
+
+    legendre = Pk
+end function legendre
+
+
+real (kind=8) function Lambda(n, x)
+    implicit none
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    real (kind=8), external :: legendre
+
+    Lambda = legendre(n-1, x) - legendre(n+1, x)
+end function Lambda
+
+
 real (kind=8) function f(x)
     implicit none
     real (kind=8), intent(in) :: x
@@ -29,7 +70,7 @@ end function f_p
 
 subroutine newton_raphson(x_i, root, debug)
     implicit none
-    integer :: n
+    integer :: iter
     real (kind=8), intent(in) :: x_i        ! initial guess value
     real (kind=8), intent(out) :: root
     logical, intent(in) :: debug
@@ -46,12 +87,12 @@ subroutine newton_raphson(x_i, root, debug)
             'n', 'x_n', 'x_{n+1}', 'err = |x_{n+1} - x_n|'
     end if
 
-    do n = 1, 100
+    do iter = 1, 100
         x_new = x_old - f(x_old) / f_p(x_old)
 
         if (debug) then
             print '(I3, 2F20.16, E25.16)', &
-                n, x_old, x_new, abs(x_new - x_old)
+                iter, x_old, x_new, abs(x_new - x_old)
         end if
 
         if (abs(x_new - x_old) .lt. (tol + rtol*abs(x_new))) then
