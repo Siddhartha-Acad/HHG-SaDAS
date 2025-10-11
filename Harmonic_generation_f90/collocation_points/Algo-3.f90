924c4a80766ea067c66113c1aa74db2c6e245fed
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-11 11:47:08 +0530

    newton function can take function f(x) and f_p(x)

--- Harmonic_generation_f90/collocation_points/Algo-3.f90
+++ Harmonic_generation_f90/collocation_points/Algo-3.f90
@@ -11,14 +11,31 @@ program main
 end program main
 
 
+real (kind=8) function f(x)
+    implicit none
+    real (kind=8), intent(in) :: x
+
+    f = sin(x)
+end function f
+
+
+real (kind=8) function f_p(x)
+    implicit none
+    real (kind=8), intent(in) :: x
+
+    f_p = cos(x)
+end function f_p
+
+
 subroutine newton_raphson(x_i, root, debug)
     implicit none
+    integer :: n
     real (kind=8), intent(in) :: x_i        ! initial guess value
     real (kind=8), intent(out) :: root
     logical, intent(in) :: debug
     real (kind=8) :: x_old, x_new
     real (kind=8) :: tol, rtol
-    integer :: n
+    real (kind=8), external :: f, f_p
 
     tol = 1.0d-14       ! absolute error tolerance
     rtol = 0.0d0        ! relative tolerance
@@ -30,7 +47,7 @@ subroutine newton_raphson(x_i, root, debug)
     end if
 
     do n = 1, 100
-        x_new = x_old - sin(x_old) / cos(x_old)
+        x_new = x_old - f(x_old) / f_p(x_old)
 
         if (debug) then
             print '(I3, 2F20.16, E25.16)', &
@@ -48,3 +65,5 @@ subroutine newton_raphson(x_i, root, debug)
     ! Reaching here is signature that convergence failed.
     root = 1.0d2
 end subroutine newton_raphson
+
+
