8899f6499c56548cc833f1e45117a8223ba01174
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-11 22:38:29 +0530

    first implementation of maximas

--- Harmonic_generation_f90/multiple_roots.f90
+++ Harmonic_generation_f90/multiple_roots.f90
@@ -1,2 +1,29 @@
 ! compute all roots of a function in a given range.
 ! using Newton-Raphson method.
+
+program main
+    implicit none
+    integer :: i
+    real (kind=8) :: dx, max
+    integer, parameter :: nop = 10000
+    real (kind=8), parameter :: xi = 0.0d0, xf = 10.0d0
+    real (kind=8), dimension(nop) :: x, y
+
+    dx = (xf - xi) / dble(nop - 1)
+
+    x(1) = xi
+    do i = 1, nop-1
+        x(i+1) = x(i) + dx
+    end do
+
+    y = sin(x)
+
+    do i = 2, nop-1
+        if (y(i-1) .lt. y(i) .and. y(i) .gt. y(i+1)) then
+            print *, x(i) / 3.14159_8
+        end if
+    end do
+
+
+
+end program main
