37751f9265265a43314d27d1bd0d20886e776f2d
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-12 09:41:11 +0530

    x array using: Fortran array constructor with an implied DO loop

--- Harmonic_generation_f90/multiple_roots.f90
+++ Harmonic_generation_f90/multiple_roots.f90
@@ -1,5 +1,5 @@
 ! compute all roots of a function in a given range.
-! using Newton-Raphson method.
+! aiming to get rough estimation.
 
 program main
     implicit none
@@ -8,15 +8,11 @@ program main
     integer, parameter :: nop = 10000
     real (kind=8), parameter :: xi = 0.0d0, xf = 10.0d0
     real (kind=8), parameter :: pi = 4.0d0 * atan(1.0d0)
-    real (kind=8), dimension(nop) :: x, y
     real (kind=8), allocatable :: roots(:)
+    real (kind=8), dimension(nop) :: x, y
+
     dx = (xf - xi) / dble(nop - 1)
-
-    x(1) = xi
-    do i = 1, nop-1
-        x(i+1) = x(i) + dx
-    end do
-
+    x = [(xi + (i-1)*dx, i=1,nop)]
     y = -sin(x)**2
 
     root_count = 0
