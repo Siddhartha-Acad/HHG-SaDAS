5f358f2bd2dd64c859b15313f22db8ec9f1c8ee5
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-13 01:35:43 +0530

    first module legendre_stuff.
    - containing: legendre, Lambda, Lambda_p
    - functions.f90 will contain all necessary functions ans subroutines.
      and all other scripts will read from this script.
    - Because I don't really wanna keep copies of same code in multiple locations.

--- Harmonic_generation_f90/collocation_points/guess_roots_LambdaN.f90
+++ Harmonic_generation_f90/collocation_points/guess_roots_LambdaN.f90
@@ -2,6 +2,7 @@
 ! aiming to get rough estimation.
 
 program main
+    use legendre_stuff
     implicit none
     real (kind=8) :: dx
     integer :: i, root_count
@@ -12,7 +13,7 @@ program main
     real (kind=8), dimension(nop) :: x, y
 
     integer, parameter :: N = 5
-    real (kind=8), external :: Lambda
+    ! real (kind=8), external :: Lambda
 
     dx = (xf - xi) / dble(nop - 1)
     x = [(xi + (i-1)*dx, i=1,nop)]
@@ -38,59 +39,3 @@ program main
     end if
 
 end program main
-
-
-pure real (kind=8) function legendre(n, x)
-    ! FORWARD RECURSION ALGORITHM
-    ! P_n(x) = \frac{1}{n}\left[ (2n-1)xP_{n-1}(x) - (n-1)P_{n-2}(x) \right]
-
-    implicit none
-    integer, intent(in) :: n
-    real (kind=8), intent(in) :: x
-    real (kind=8) :: P0, P1, Pk
-    integer :: k
-
-    P0 = 1.0d0
-    P1 = x
-
-    if (n==0) then
-        legendre = 1.0d0
-        return
-    else if (n==1) then
-        legendre = x
-        return
-    end if
-
-    do k = 2, n
-        Pk = ((2*k-1.0d0)*x*P1 - (k-1.0d0)*P0) / k
-        P0 = P1
-        P1 = Pk
-    end do
-
-    legendre = Pk
-end function legendre
-
-
-real (kind=8) function Lambda(n, x)
-    implicit none
-    integer, intent(in) :: n
-    real (kind=8), intent(in) :: x
-    real (kind=8), external :: legendre
-
-    Lambda = legendre(n-1, x) - legendre(n+1, x)
-end function Lambda
-
-
-real (kind=8) function Lambda_p(n, x)
-    implicit none
-    integer, intent(in) :: n
-    real (kind=8), intent(in) :: x
-    real (kind=8), external :: Lambda
-    real (kind=8) :: coff_1, coff_2
-
-    coff_1 = dble(n*(n-1)) / dble(2*n-1)
-    coff_2 = dble((n+1)*(n+2)) / dble(2*n+3)
-
-    Lambda_p = (coff_1*Lambda(n-1, x) - &
-                coff_2*Lambda(n+1, x)) / (1.0d0 - x**2)
-end function Lambda_p
---
+++ Harmonic_generation_f90/functions.f90
@@ -0,0 +1,66 @@
+! This script will contain the functions and subroutines
+! that will be useful in other scripts...
+
+module legendre_stuff
+    implicit none
+
+contains
+
+pure real (kind=8) function legendre(n, x)
+    ! FORWARD RECURSION ALGORITHM
+    ! P_n(x) = \frac{1}{n}\left[ (2n-1)xP_{n-1}(x) - (n-1)P_{n-2}(x) \right]
+
+    implicit none
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    real (kind=8) :: P0, P1, Pk
+    integer :: k
+
+    P0 = 1.0d0
+    P1 = x
+
+    if (n==0) then
+        legendre = 1.0d0
+        return
+    else if (n==1) then
+        legendre = x
+        return
+    end if
+
+    do k = 2, n
+        Pk = ((2*k-1.0d0)*x*P1 - (k-1.0d0)*P0) / k
+        P0 = P1
+        P1 = Pk
+    end do
+
+    legendre = Pk
+end function legendre
+
+
+real (kind=8) function Lambda(n, x)
+    implicit none
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    ! real (kind=8), external :: legendre
+
+    Lambda = legendre(n-1, x) - legendre(n+1, x)
+end function Lambda
+
+
+real (kind=8) function Lambda_p(n, x)
+    implicit none
+    integer, intent(in) :: n
+    real (kind=8), intent(in) :: x
+    ! real (kind=8), external :: Lambda
+    real (kind=8) :: coff_1, coff_2
+
+    coff_1 = dble(n*(n-1)) / dble(2*n-1)
+    coff_2 = dble((n+1)*(n+2)) / dble(2*n+3)
+
+    Lambda_p = (coff_1*Lambda(n-1, x) - &
+                coff_2*Lambda(n+1, x)) / (1.0d0 - x**2)
+end function Lambda_p
+
+
+
+end module legendre_stuff
