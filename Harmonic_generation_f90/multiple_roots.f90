c518228c0040f028ae212fc1e0005d6e6bb30cd0
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-12 09:15:26 +0530

    roots of function saved in array
    - first time used allocatable arrays..

--- Harmonic_generation_f90/multiple_roots.f90
+++ Harmonic_generation_f90/multiple_roots.f90
@@ -3,12 +3,13 @@
 
 program main
     implicit none
-    integer :: i
-    real (kind=8) :: dx, max
+    real (kind=8) :: dx
+    integer :: i, root_count
     integer, parameter :: nop = 10000
     real (kind=8), parameter :: xi = 0.0d0, xf = 10.0d0
+    real (kind=8), parameter :: pi = 4.0d0 * atan(1.0d0)
     real (kind=8), dimension(nop) :: x, y
-
+    real (kind=8), allocatable :: roots(:)
     dx = (xf - xi) / dble(nop - 1)
 
     x(1) = xi
@@ -16,14 +17,22 @@ program main
         x(i+1) = x(i) + dx
     end do
 
-    y = sin(x)
+    y = -sin(x)**2
+
+    root_count = 0
+    allocate(roots(0))
 
     do i = 2, nop-1
         if (y(i-1) .lt. y(i) .and. y(i) .gt. y(i+1)) then
-            print *, x(i) / 3.14159_8
+            root_count = root_count + 1
+            roots = [roots, x(i)]
+            print *, x(i) / pi
         end if
     end do
 
-
+    print *, allocated(roots)
+    if (allocated(roots)) then
+        deallocate(roots)
+    end if
 
 end program main
