1d1c5cd4024d9cf9597f0eec59dec37aad51b5c4
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-18 12:21:55 +0530

    Learning makefile from Dr. Gaurav Bhutani.
    youtube link : https://www.youtube.com/watch?v=sg14LA4Gnc4
    slides link  : https://lms.iitmandi.ac.in/pluginfile.php/178415/mod_resource/content/0/HPSC-Lec17.pdf
    
    PS HHG-SaDAS> make --version
    GNU Make 4.4.1
    Built for Windows32
    Copyright (C) 1988-2023 Free Software Foundation, Inc.
    License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>
    This is free software: you are free to change and redistribute it.
    There is NO WARRANTY, to the extent permitted by law.

---
+++ Harmonic_generation_f90/built-system/main.f90
@@ -0,0 +1,8 @@
+! Created by Siddhartha Mithiya on 18-10-2025.
+
+program demo
+    print *, "this is main program"
+    call sub1()
+    call sub2()
+
+end program demo
---
+++ Harmonic_generation_f90/built-system/sub1.f90
@@ -0,0 +1,5 @@
+! Created by Siddhartha Mithiya on 18-10-2025.
+
+subroutine sub1()
+    print *, "In sub1"
+end subroutine sub1
