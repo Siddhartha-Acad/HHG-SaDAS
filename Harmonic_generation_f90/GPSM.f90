b594400846d984556016cdd04caaa9f662032749
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-24 10:54:42 +0530

    Initiated GPSM implementation in fortran.

---
+++ Harmonic_generation_f90/GPSM.f90
@@ -0,0 +1,10 @@
+! fortran implementation for solving time independent Schrodinger equation using GPSM
+
+program GPSM
+    implicit none
+    integer, parameter :: N = 200
+    real(kind=8), dimension(N-1) :: rx
+
+
+
+end program GPSM
