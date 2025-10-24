aa326ff3de486f77775590b37e835b0547dc2668
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-24 20:17:51 +0530

    implemented d2(i, j) function as internal function.

--- Harmonic_generation_f90/GPSM.f90
+++ Harmonic_generation_f90/GPSM.f90
@@ -11,13 +11,27 @@ program GPSM
     !~~~~~~~~~: reading collocation points :~~~~~~~~~
     open(unit=10, file='./collocation_points/generator/Algo-3_N=200_Gauss_Lobatto_collocation_points.dat', &
          status='old', action='read')
-
+    
     do i=1, N-1
         read(10, *) x(i)
     end do
     close(10)
     !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print *, f_p(x(1))
+    
+    print *, d2(1, N-1)
+    
+    
+contains
+    pure real(kind=8) function d2(i, j)
+        implicit none
+        integer, intent(in) :: i, j
+        
+        if (i .ne. j) then
+            d2 = -2 / (x(i) - x(j))**2
+        else
+            d2 = -N*(N+1) / (3*(1 - x(i)**2))
+        end if
+    end function d2
     
 
 end program GPSM
@@ -32,3 +46,4 @@ pure real(kind=8) function f_p(x)
 	alpha_map = 2 * Lmap / r_max
     f_p = Lmap * (alpha_map + 2) / (1 - x + alpha_map)**2
 end function f_p
+
