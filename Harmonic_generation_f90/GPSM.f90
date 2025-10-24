2e09ce94912b8aa50bf668da67d6535b976cfd19
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-24 12:14:05 +0530

    Read collocation points and save to x array.

--- Harmonic_generation_f90/GPSM.f90
+++ Harmonic_generation_f90/GPSM.f90
@@ -1,10 +1,23 @@
-! fortran implementation for solving time independent Schrodinger equation using GPSM
+! fortran implementation for solving 
+! time independent Schrodinger equation using GPSM
 
 program GPSM
     implicit none
+    integer :: i
     integer, parameter :: N = 200
-    real(kind=8), dimension(N-1) :: rx
+    real(kind=8), dimension(N-1) :: x   ! collocation points
 
+    !~~~~~~~~~: reading collocation points :~~~~~~~~~
+    open(unit=10, file='./collocation_points/generator/Algo-3_N=200_Gauss_Lobatto_collocation_points.dat', &
+         status='old', action='read')
+
+    do i=1, N-1
+        read(10, *) x(i)
+    end do
+
+    close(10)
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print *, x(199)
 
 
 end program GPSM
