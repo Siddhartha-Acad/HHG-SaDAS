2d7c69d272c631f02dd22c78387d743dd024ad23
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-28 18:47:17 +0530

    start implementing the S-matrix.

--- Harmonic_generation_f90/parameters.f90
+++ Harmonic_generation_f90/parameters.f90
@@ -12,6 +12,7 @@ module parameters
     !                GPSM Parameters                 |
     ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     integer, parameter :: N = 200        ! P'_N(xj) = 0 ; radial grid size: len(colloc_pt) = N-1
+    integer, parameter :: l_max = 20     ! Number of partial waves = number of S-matrices = l_max+1
     integer, parameter :: kmax = 5       ! number of GPSM states (maximum k index) in S matrix
     real(kind=8), parameter :: Lmap = 80.0d0, r_max = 200.0d0    ! radial mapping parameters
     real(kind=8), parameter :: alpha_map = 2.0d0 * Lmap / r_max
