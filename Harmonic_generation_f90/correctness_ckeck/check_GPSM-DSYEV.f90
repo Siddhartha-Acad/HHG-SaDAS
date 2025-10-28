aaff1908cbbccf7a6208b1706cd060bb9b3e9a07
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-10-28 09:37:18 +0530

    compare theoretical radius and radius from GPSM.
     n     radius_Th           radius_GPSM
     1    1.50000000    1.4999999999996707
     2    6.00000000    5.9999999999992903
     3   13.50000000   13.4999999999986198
     4   24.00000000   23.9999999999989306
     5   37.50000000   37.4999999999984297

--- Harmonic_generation_f90/correctness_ckeck/check_GPSM-DSYEV.f90
+++ Harmonic_generation_f90/correctness_ckeck/check_GPSM-DSYEV.f90
@@ -1,20 +1,22 @@
 program read_GPSM_data
     implicit none
-
     integer :: i
-    integer, parameter :: N = 200, kmax = 5
-    real(kind=8), dimension(N-1) :: rx
+    integer, parameter :: N = 200, kmax = 5, l = 0
+    real :: rad_theory
+    real(kind=8), dimension(N-1) :: r
     real(kind=8), dimension(N-1, kmax) :: A
 
     open(unit=12, file='../GPSM-DSYEV_states.bin', form='unformatted', access='stream', status='old')
-
-    do i = 1, N-1
-        read(12) rx(i), A(i, 1:kmax)
-    end do
-
+        do i = 1, N-1
+            read(12) r(i), A(i, 1:kmax)
+        end do
     close(12)
-
-    print *, sum(rx * A(:, 1) ** 2)
+    
+    print '(A2, A14, A22)', 'n', 'radius_Th', 'radius_GPSM'
+    do i = 1, kmax
+        rad_theory = 0.5 * real(3*i**2 - l*(l+1))
+        print '(I2, F14.8, F22.16)', i, rad_theory, sum(r * A(:, i)**2)
+    end do
 
 end program read_GPSM_data
 
