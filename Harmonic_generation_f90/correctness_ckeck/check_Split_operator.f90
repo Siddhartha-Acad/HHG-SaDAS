f63691c6b23cba23b4c0b0fa400aaae5782b5986
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-11-08 15:53:03 +0530

    print check_split_operator results in preety format
    siddhartha@linux: correctness_ckeck$ ./a.out
    ------------------------------------------------------
       S @ A     A(1)     A(2)     A(3)     A(4)     A(5)
    ------------------------------------------------------
        S(1)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(2)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(3)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(4)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(5)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(6)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(7)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(8)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
        S(9)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(10)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(11)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(12)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(13)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(14)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(15)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(16)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(17)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(18)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(19)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(20)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
       S(21)    [PASS]   [PASS]   [PASS]   [PASS]   [PASS]
    ------------------------------------------------------
    
    Note:
      err = max( | S(l) @ A(n, l) - A(n, l) | )
      [PASS] : err < 1.0d-3
      [FAIL] : err > 1.0d-3
    ------------------------------------------------------

--- Harmonic_generation_f90/correctness_ckeck/check_Split_operator.f90
+++ Harmonic_generation_f90/correctness_ckeck/check_Split_operator.f90
@@ -6,12 +6,16 @@ program check_Split_operator
     complex(kind=8) :: S_matrix(N-1, N-1)
 
     real(kind=8) :: max_abs_err
-    real(kind=8), dimension(N-1) :: r, A_tilde
+    real(kind=8), dimension(N-1) :: A_tilde
     real(kind=8), dimension(N-1, check_n_states) :: A
 
-    character(len=*), parameter :: green = char(27)//'[1;32m'  ! bold + green
-    character(len=*), parameter :: red   = char(27)//'[1;31m'  ! bold + red
-    character(len=*), parameter :: reset = char(27)//'[0m'     ! reset style
+    integer, parameter :: CW = 8         ! column width (adjust if you want wider)
+    character(len=CW) :: col             ! temporary column field
+    character(len=8)  :: inum            ! enough for the integer -> text
+    character(len=20) :: fmtA            ! format string like '(A5)'
+    character(len=*), parameter :: green = char(27)//'[1;32m', &
+                                   red   = char(27)//'[1;31m', &
+                                   reset = char(27)//'[0m'
 
     inquire(iolength=S_recl_size) S_matrix
     inquire(iolength=A_recl_size) A
@@ -21,29 +25,49 @@ program check_Split_operator
     open(unit=11, file='../GPSM_states_S-matrix/data_GPSM_states_S-matrix/S_matrices-DSYEV.bin', &
             form='unformatted', access='direct', recl=S_recl_size, status='old')
 
-    do l_val = m_qn, l_max + m_qn
 
-    read(10, rec=l_val - m_qn + 1) A
-    read(11, rec=l_val - m_qn + 1) S_matrix        ! rec = 1, 2, 3, ..., l_max
+    write(fmtA, '(A,I0,A)') '(A', CW, ')'                       ! format string dynamically based on CW
+    print '(A)', repeat('-', (1 + check_n_states) * (CW + 1))   ! Horizontal rule
 
-    print '(A11, A, A11, A, A6)', 'S(l) @ A(n)', ' | ', 'abs_max_err', ' | ', 'status'
-    print '(A)', repeat('-', 35)
-
+    write(*, fmtA, advance='no') 'S @ A'
     do i = 1, check_n_states
-        A_tilde = matmul(S_matrix, cmplx(A(:, i), 0.0d0, kind=8))
-        max_abs_err = maxval(abs(A_tilde - A(:, i)))
+        write(inum,'(I0)') i
+        write(*,'(1X'//trim(fmtA)//')', advance='no') 'A(' // trim(inum) // ')'
+    end do
+    print *
+    print '(A)', repeat('-', (1 + check_n_states) * (CW + 1))
 
-        if (max_abs_err .lt. 1.0d-3) then
-            write(*,'(A,I0,A,I0,A,ES11.3,A,A)', advance='no') &
-                'S(', m_qn, ') @ A(', i, ') | ', max_abs_err, ' | ', green//'[PASS]'//reset
-            print *  ! newline after pass/fail
-        else
-            write(*,'(A,I0,A,I0,A,ES11.3,A,A)', advance='no') &
-                'S(', m_qn, ') @ A(', i, ') | ', max_abs_err, ' | ', red//'[FAIL]'//reset
-            print *  ! newline after pass/fail
-        end if
+
+    do l_val = m_qn, l_max + m_qn
+        read(10, rec=l_val - m_qn + 1) A
+        read(11, rec=l_val - m_qn + 1) S_matrix
+
+        write(inum,'(I0)') l_val
+        write(*, '('//trim(fmtA)//',1X)', advance='no') 'S(' // trim(inum) // ')'
+
+        do i = 1, check_n_states
+            A_tilde = matmul(S_matrix, cmplx(A(:, i), 0.0d0, kind=8))
+            max_abs_err = maxval(abs(A_tilde - A(:, i)))
+            if (max_abs_err < 1.0d-3) then
+                write(col, fmtA) '[PASS]'
+                write(*,'(1X,A)', advance='no') green // trim(col) // reset
+            else
+                write(col, fmtA) '[FAIL]'
+                write(*,'(1X,A)', advance='no') red // trim(col) // reset
+            end if
+        end do
+        print *
     end do
-    end do
+
     close(10)
     close(11)
+
+    print '(A)', repeat('-', (1 + check_n_states) * (CW + 1))
+    print *
+    print '(A)', 'Note:'
+    print '(A)', '  err = max( | S(l) @ A(n, l) - A(n, l) | )'
+    print '(A)', '  ' // green // '[PASS]' // reset // ' : err < 1.0d-3'
+    print '(A)', '  ' // red   // '[FAIL]' // reset // ' : err > 1.0d-3'
+    print '(A)', repeat('-', (1 + check_n_states) * (CW + 1))
+
 end program check_Split_operator
