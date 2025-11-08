0640d3b2835517a27707dbd9e4b91b17196f1015
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-11-08 09:52:11 +0530

    checking split-operator for all wavefunctions.

--- Harmonic_generation_f90/correctness_ckeck/check_GPSM.f90
+++ Harmonic_generation_f90/correctness_ckeck/check_GPSM.f90
@@ -3,6 +3,7 @@ program read_GPSM_data
     implicit none
     integer :: i
     real :: rad_theory
+    real(kind=8) :: rad_gpsm, rel_err
     real(kind=8), dimension(N-1) :: r
     real(kind=8), dimension(N-1, total_states) :: A
 
@@ -11,11 +12,21 @@ program read_GPSM_data
         read(12) r, A
     close(12)
 
-    print '(A2, A14, A22)', 'n', 'radius_Th', 'radius_GPSM'
+    print '(A3, A, A11, A, A18, A, A10, A, A6)', ' n', ' | ', 'radius_Th', ' | ', 'radius_GPSM', ' | ', 'rel_err', ' | ', 'status'
+    print '(A)', repeat('-', 62)
+
     do i = 1, total_states
         rad_theory = 0.5 * real(3*i**2 - l_qn*(l_qn+1))
-        print '(I2, F14.8, F22.16)', i, rad_theory, sum(r * A(:, i)**2)
+        rad_gpsm = sum(r * A(:, i)**2)
+        rel_err = abs(rad_gpsm - rad_theory) / rad_theory
+
+        if (rel_err .lt. 1.0d-3) then
+            print '(I3, A, F11.6, A, F18.13, A, ES10.2, A, A)', & 
+                    i, ' | ', rad_theory, ' | ', rad_gpsm, ' | ', rel_err, ' | ', '[PASS]'
+        else
+            print '(I3, A, F11.6, A, F18.13, A, ES10.2, A, A)', &
+                    i, ' | ', rad_theory, ' | ', rad_gpsm, ' | ', rel_err, ' | ', '[FAIL]'
+        end if
     end do
 
 end program read_GPSM_data
-
--- Harmonic_generation_f90/correctness_ckeck/check_Split_operator.f90
+++ Harmonic_generation_f90/correctness_ckeck/check_Split_operator.f90
@@ -6,12 +6,11 @@ program check_Split_operator
     integer, parameter :: l_ind = 1             ! select S_matrix, compatible with GPSM l_qn.
     complex(kind=8) :: S_matrix(N-1, N-1)
 
-    real(kind=8) :: rel_err
+    real(kind=8) :: max_abs_err
     real(kind=8), dimension(N-1) :: r, A_tilde
     real(kind=8), dimension(N-1, total_states) :: A
 
     inquire(iolength=recl_size) S_matrix            ! "How many units does S_matrix need?"
-    print *, "S-Matrix Record length:", recl_size
 
     open(unit=10, file='../GPSM_states_S-matrix/data_GPSM_states_S-matrix/GPSM-DSYEV_states.bin', &
             form='unformatted', access='stream', status='old')
@@ -23,13 +22,18 @@ program check_Split_operator
         read(11, rec=l_ind) S_matrix        ! rec = 1, 2, 3, ..., l_max
     close(11)
 
-    A_tilde = matmul(S_matrix, cmplx(A(:, 1), 0.0d0, kind=8))
-    rel_err = sum(abs(A_tilde**2 - A(:, 1)**2) / abs(A(:, 1)**2)) / (N-1)
+    print '(A11, A, A11, A, A6)', 'S(l) @ A(n)', ' | ', 'abs_max_err', ' | ', 'status'
+    print '(A)', repeat('-', 35)
 
-    print *, ''
-    print *, '=== Error Analysis ==='
-    print *, 'Max absolute error:     ', maxval(abs(A_tilde - A(:, 1)))
-    print *, 'Relative error (norms): ', abs(norm2(A_tilde) - norm2(A(:, 1))) / norm2(A(:, 1))
+    do i = 1, total_states
+        A_tilde = matmul(S_matrix, cmplx(A(:, i), 0.0d0, kind=8))
+        max_abs_err = maxval(abs(A_tilde - A(:, i)))
 
+        if (max_abs_err .lt. 1.0d-3) then
+            print '(A,I0,A,I0,A,A,ES11.3,A,A)', 'S(', l_ind-1, ') @ A(', i, ')', ' | ', max_abs_err, ' | ', '[PASS]'
+        else
+            print '(A,I0,A,I0,A,A,ES11.3,A,A)', 'S(', l_ind-1, ') @ A(', i, ')', ' | ', max_abs_err, ' | ', '[FAIL]'
+        end if
+    end do
 
 end program check_Split_operator
